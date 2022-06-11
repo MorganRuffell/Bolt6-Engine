d9e96d07785b270ae2b16b5d23f6e9a25ba92436
Morgan <morgan.ruffell@icloud.com>
2022-06-11 17:36:12 +0100

    Building the renderer, going to start simple

--- Core/Core/DirectX11RendererComponent.cpp
+++ Core/Core/DirectX11RendererComponent.cpp
@@ -1,1 +1,34 @@
 #include "DirectX11RendererComponent.h"
+#include <assert.h>
+
+DirectX11RendererComponent::DirectX11RendererComponent(Accelerator* Accel)
+{
+    assert(Accel != nullptr);
+
+    InitalizeRasterizerStates(Accel);
+}
+
+void DirectX11RendererComponent::InitalizeComponent()
+{
+}
+
+bool DirectX11RendererComponent::TerminateComponent()
+{
+    return false;
+}
+
+void DirectX11RendererComponent::InitalizeRasterizerStates(Accelerator* Accel)
+{
+
+    StandardRS = new RasterizerState();
+    WireframeRS = new RasterizerState();
+
+    StandardRS->InitalizeState(D3D11_FILL_SOLID, D3D11_CULL_BACK, Accel);
+    WireframeRS->InitalizeState(D3D11_FILL_WIREFRAME, D3D11_CULL_NONE, Accel);
+}
+
+void DirectX11RendererComponent::DestroyRasterizerStates()
+{
+    delete StandardRS;
+    delete WireframeRS;
+}
--- Core/Core/DirectX11RendererComponent.h
+++ Core/Core/DirectX11RendererComponent.h
@@ -1,9 +1,42 @@
 #pragma once
 
 #include "BaseRendererComponent.h"
+#include "RasterizerState.h"
+#include <memory>
 
 class DirectX11RendererComponent : public BaseRendererComponent
 {
+public:
+
+	DirectX11RendererComponent(Accelerator* Accel);
+
+	virtual void InitalizeComponent() override;
+	virtual bool TerminateComponent() override;
+
+protected:
+
+	void InitalizeRasterizerStates(Accelerator* Accel);
+	void DestroyRasterizerStates();
+
+
+protected:
+
+	RasterizerState* StandardRS;
+	RasterizerState* WireframeRS;
+
+
+private:
+
+	int AmountOfRasterizerStates;
+
+protected:
+
+	ID3D11BlendState*				m_BlendState;
+	ID3D11DepthStencilState*		m_DepthStencilState;
+
+protected:
+
+	D3D11_PRIMITIVE_TOPOLOGY primitiveTopology;
 
 };
 
